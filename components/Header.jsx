6f731b9bb5569a08548fca8ab0203d9441924b56
G808 <hucciallstars@gmail.com>
2023-02-22 21:59:54 +0200

    Installed react-icons, chart.js

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="flex justify-between px-4 pt-4">
+      <h2>Dashboard</h2>
+      <h2>Welcome back, user</h2>
+    </div>
+  );
 };
 
 export default Header;
--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className=" bg-red-400 ">
+      <main className="min-h-screen bg-red-200">
         <Header />
       </main>
     </>
