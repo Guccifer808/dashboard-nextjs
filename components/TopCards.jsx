93c75f124e93a012ad7aa01b95258bbd1c9e0c38
G808 <hucciallstars@gmail.com>
2023-02-23 01:30:51 +0200

    TopCards componend edits

--- components/TopCards.jsx
+++ components/TopCards.jsx
@@ -8,26 +8,26 @@ const TopCards = () => {
           <p className="text-2xl font-bold">$8,8548</p>
           <p className="text-gray-700">Daily Revenue</p>
         </div>
-        <p className="my-2 flex items-center justify-center rounded-lg bg-green-200 p-2 font-semibold">
+        <p className="my-1 flex min-w-[55px] items-center justify-center rounded-lg bg-green-200 p-2 font-semibold">
           <span>+8%</span>
         </p>
       </div>
       <div className="col-span-1 flex w-full justify-between rounded-lg border bg-white p-4 lg:col-span-2">
         <div className="flex w-full flex-col pb-4">
-          <p className="text-2xl font-bold">$8,8548</p>
-          <p className="text-gray-700">Daily Revenue</p>
+          <p className="text-2xl font-bold">$458,851</p>
+          <p className="text-gray-700">Total Revenue</p>
         </div>
-        <p className="my-2 flex items-center justify-center rounded-lg bg-green-200 p-2 font-semibold">
-          <span>+8%</span>
+        <p className="my-1 flex min-w-[55px] items-center justify-center rounded-lg bg-green-200 p-2 font-semibold">
+          <span>+2%</span>
         </p>
       </div>
       <div className="flex w-full justify-between rounded-lg border bg-white p-4">
         <div className="flex w-full flex-col pb-4">
-          <p className="text-2xl font-bold">$8,8548</p>
-          <p className="text-gray-700">Daily Revenue</p>
+          <p className="text-2xl font-bold">1752</p>
+          <p className="text-gray-700">Customers</p>
         </div>
-        <p className="my-2 flex items-center justify-center rounded-lg bg-green-200 p-2 font-semibold">
-          <span>+8%</span>
+        <p className="my-1 flex min-w-[55px] items-center justify-center rounded-lg bg-green-200 p-2 font-semibold">
+          <span>+11%</span>
         </p>
       </div>
     </div>
