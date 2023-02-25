f648e5f0ea122111870eb7c20a468c5d9225de58
G808 <hucciallstars@gmail.com>
2023-02-26 00:08:52 +0200

    orders page & small edits

--- pages/customers.jsx
+++ pages/customers.jsx
@@ -5,19 +5,19 @@ import { BsThreeDotsVertical } from "react-icons/bs";
 const customers = () => {
   return (
     <div className="min-h-screen bg-slate-100">
-      <div className="flex justify-between p-4">
+      <div className="flex justify-between px-4 pt-4">
         <h2>Customers</h2>
         <h2>Welcome back, user</h2>
       </div>
       <div className="p-4">
-        <div className="m-auto w-full overflow-y-auto rounded-lg border bg-white p-1">
+        <div className="m-auto w-full overflow-y-auto rounded-lg border bg-white p-4">
           <div className="my-3 grid cursor-pointer grid-cols-2 items-center justify-center gap-2 p-2 sm:grid-cols-2 md:grid-cols-3 md:justify-between lg:grid-cols-4">
             <span className="text-left">Name</span>
             <span className="hidden text-right sm:grid sm:text-left">
               Email
             </span>
             <span className="hidden lg:grid">Last Order</span>
-            <span className="hidden md:flex md:justify-end">Method</span>
+            <span className="hidden sm:grid">Method</span>
           </div>
           <ul>
             {mockData.map((order, id) => (
@@ -38,7 +38,7 @@ const customers = () => {
                 </p>
                 <p className="hidden lg:flex">{order.date}</p>
 
-                <div className="hidden items-center justify-between md:flex md:justify-end lg:flex">
+                <div className="hidden items-center justify-between sm:flex">
                   <p>{order.method}</p>
                   <BsThreeDotsVertical />
                 </div>
--- pages/orders.jsx
+++ pages/orders.jsx
@@ -1,7 +1,66 @@
-import React from "react";
+import { GiShoppingBag } from "react-icons/gi";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import { mockData } from "@/mockData/mockData";
 
 const orders = () => {
-  return <div>orders</div>;
+  return (
+    <div className="min-h-screen bg-slate-100">
+      <div className="flex justify-between px-4 pt-4">
+        <h2>Orders</h2>
+        <h2>Welcome back, user</h2>
+      </div>
+      <div className="p-4">
+        <div className="m-auto w-full overflow-y-auto rounded-lg border bg-white p-4">
+          <div className="my-3 grid cursor-pointer grid-cols-2 items-center justify-between p-2 sm:grid-cols-3 md:grid-cols-4">
+            <span>Order</span>
+            <span className="text-right sm:text-left">Status</span>
+            <span className="hidden md:grid">Last Order</span>
+            <span className="hidden sm:grid">Method</span>
+          </div>
+          <ul>
+            {mockData.map((order, id) => (
+              <li
+                key={id}
+                className="my-3 grid grid-cols-2 items-center justify-between rounded-lg bg-slate-50 p-2 hover:bg-slate-100 sm:grid-cols-3 md:grid-cols-4"
+              >
+                <div className="flex">
+                  {/* Icon wrapper */}
+                  <div className="rounded-lg bg-rose-100 p-3">
+                    <GiShoppingBag className="text-rose-500" />
+                  </div>
+                  <div className="pl-4">
+                    <p className="font-bold text-slate-700">
+                      ${order.total.toLocaleString()}
+                    </p>
+                    <p className="text-sm text-slate-700">{order.name.first}</p>
+                  </div>
+                </div>
+                {/* Status */}
+                <p className="text-right text-slate-600 sm:text-left">
+                  <span
+                    className={
+                      order.status == "Completed"
+                        ? "rounded-lg bg-green-200 p-2"
+                        : order.status == "Pending"
+                        ? "rounded-lg bg-blue-200 p-2"
+                        : "rounded-lg bg-yellow-200 p-2"
+                    }
+                  >
+                    {order.status}
+                  </span>
+                </p>
+                <p className="hidden md:flex">{order.date}</p>
+                <div className="hidden items-center justify-between sm:flex">
+                  <p>{order.method}</p>
+                  <BsThreeDotsVertical />
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default orders;
