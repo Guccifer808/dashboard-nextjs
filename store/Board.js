271f206586d8e2c1289090947fc1706facdaebaf
G808 <hucciallstars@gmail.com>
2023-03-06 15:54:15 +0200

    Removed unused deps

--- store/Board.js
+++
@@ -1,9 +0,0 @@
-import { create } from "zustand";
-import { boardData } from "../mockData";
-
-const useBoard = create((set) => ({
-  board: boardData,
-  setBoard: (board) => set((state) => ({ board })),
-}));
-
-export default useBoard;
